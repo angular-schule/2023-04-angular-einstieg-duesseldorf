3d9347c104b33c921f77920a459f13c687c6de37
Ferdinand Malcher <ferdinand@malcher.media>
2023-04-24 16:13:26 +0200

    Routing

--- src/app/books/books.module.ts
+++ src/app/books/books.module.ts
@@ -5,12 +5,14 @@ import { BooksRoutingModule } from './books-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { BookComponent } from './book/book.component';
 import { CartDisplayComponent } from '../cart-display/cart-display.component';
+import { BookDetailsComponent } from './book-details/book-details.component';
 
 
 @NgModule({
   declarations: [
     DashboardComponent,
-    BookComponent
+    BookComponent,
+    BookDetailsComponent
   ],
   imports: [
     CommonModule,
